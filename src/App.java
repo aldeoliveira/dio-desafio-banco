2f4037c1940e758ef7fec62aed8395de9445a995
André <aldeoliveira@gmail.com>
2024-06-24 12:30:05 -0300

    adicionar checagens de conta poupança e impressão e de extratos

--- src/App.java
+++ src/App.java
@@ -1,10 +1,10 @@
 import banco.Banco;
 import banco.cliente.Cliente;
 import banco.contas.ContaCorrente;
+import banco.contas.ContaPoupanca;
 
 public class App {
     public static void main(String[] args) throws Exception {
-
         // Criar conta
         Banco meuBanco = new Banco("Meu Banco Digital");
         Cliente fulano = meuBanco.registrarCliente("Fulano");
@@ -20,6 +20,23 @@ public class App {
         System.out.println(conta1.getSaldo());
 
         // Fazer transferência entre contas
+        Cliente beltrano = meuBanco.registrarCliente("Beltrano");
+        ContaCorrente conta2 = meuBanco.criarContaCorrente(beltrano);
+        System.out.println(conta2);
+        conta2.depositar(400);
+        conta2.transferir(conta1, 50);
+        System.out.println(conta1.getSaldo());
+        System.out.println(conta2.getSaldo());
 
+        // Criar conta poupança
+        ContaPoupanca conta3 = meuBanco.criarContaPoupanca(fulano);
+        conta3.getNumero();
+        conta1.transferir(conta3, 200);
+        conta3.getSaldo();
+
+        // Imprimir extratos das contas
+        conta1.imprimirExtrato();
+        conta2.imprimirExtrato();
+        conta3.imprimirExtrato();
     }
 }
